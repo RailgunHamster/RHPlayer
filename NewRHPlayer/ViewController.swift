056d1e76c8d5427a7fef62a5738131e95141bb36
RailgunHamster <31252310+RailgunHamster@users.noreply.github.com>
2017-10-30 16:38:29 +0800

    Initial Commit

---
+++ NewRHPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewRHPlayer
+//
+//  Created by 王宇鑫 on 2017/10/30.
+//  Copyright © 2017年 王宇鑫. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
